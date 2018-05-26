e036d65e0c91b65a373f6831594b81574a158592
Glitch (orange-suit) <none>
2018-05-26 05:19:57 +0000

    💇🐂 Checkpoint ./server.js:1200955/2608

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 
 // your code here!
-app.get('/', (req, res) => res.json({
-    "host": "your-glitch-subdomain.glitch.me",
+app.use(express.json());
+req.params: {
+    "host": "https://orange-suit.glitch.me/",
     "query": {
         "cat": "meow",
         "dog": "woof"
     },
     "params": {
       "what": "pets"
-    }
-}));
+    }}
+
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  res.send(req.params)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
